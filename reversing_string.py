96dd020ccaa750a8fa9b06d18f3adbe4eb72741c
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2013-07-22 21:35:17 +0530

    Reversing String
    First commit

---
+++ reversing_string.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Name:        Reversing a String
+# Purpose:     Different ways to reverse a string in python
+#
+# Author:      Kuldeep Rishi
+#
+# Created:     22-07-2013
+#-------------------------------------------------------------------------------
+
+string = "Hello, World!"
+print "Original String","\t"*6,"==>  ", string
+print "="*59
+
+# Method 1  :  Define a function
+# =================================
+def reverse(string):
+    result = ""
+    for i in xrange(len(string), 0, -1):
+        result = result+string[i-1] # i-1 so as to avoid out of index error
+    return result
+
+print "Method 1 : (Defining reverse function)","\t"*1,"==>  ", reverse(string)
+
+
+# Method 2  :  Using Join
+# =================================
+print "Method 2 : (Using join)","\t"*4,"==>  ", "".join([string[i-1] for i in xrange(len(string), 0, -1)])
+
+
+# Method 3  :  Using Extended Slice
+# =================================
+print "Method 3 : (Using Extended Slice)","\t"*2,"==>  ", string[::-1]
+
+
+# Method 4  :  Using Inbuilt function
+# =================================
+print "Method 4 : (Using Inbuilt function)","\t"*1,"==>  ", reverse(string)
+
