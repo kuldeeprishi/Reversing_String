ee9ab4031c121716d995836059963756dcd5ba14
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2013-07-22 21:50:31 +0530

    Reversing String
    join with reversed added

--- reversing_string.py
+++ reversing_string.py
@@ -27,12 +27,17 @@ print "Method 1 : (Defining reverse function)","\t"*1,"==>  ", reverse(string)
 print "Method 2 : (Using join)","\t"*4,"==>  ", "".join([string[i-1] for i in xrange(len(string), 0, -1)])
 
 
-# Method 3  :  Using Extended Slice
+# Method 3  :  Using Join
 # =================================
-print "Method 3 : (Using Extended Slice)","\t"*2,"==>  ", string[::-1]
+print "Method 3 : (Using join with reversed)","\t"*1,"==>  ", "".join(reversed(string))
 
 
-# Method 4  :  Using Inbuilt function
+# Method 4  :  Using Extended Slice
 # =================================
-print "Method 4 : (Using Inbuilt function)","\t"*1,"==>  ", reverse(string)
+print "Method 4 : (Using Extended Slice)","\t"*2,"==>  ", string[::-1]
+
+
+# Method 5  :  Using Inbuilt function
+# =================================
+print "Method 5 : (Using Inbuilt function)","\t"*1,"==>  ", reverse(string)
 
